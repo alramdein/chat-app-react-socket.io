80d2ff9ec578838589aaa3f1b735c36712f06764
alramdein <ramdanialif26@gmail.com>
2020-11-30 19:53:30 +0700

    bi-directional communication v.01

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
+
+import ChatRoom from '../src/components/ChatRoom/ChatRoom'
 
 function App() {
   return (
     <div className="App">
-
+      <Router>
+        <Switch>
+          <Route path="/" component={ChatRoom}/>
+        </Switch>
+      </Router>
     </div>
   );
 }
